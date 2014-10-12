575cbdf4f59c1850f29135e7f3a3c070bfce949f
richhelton <rich.helton@state.co.us>
2014-10-12 17:38:52 -0600

    3496EN_Chap04_Code

---
+++ Payment_WCFService/WCFService/WCFService/Handlers/PayHandlers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Messages;
+using PaymentMessages.IMessages;
+using NServiceBus;
+
+namespace WCFServer.Handlers
+{
+    public class PayHandlers : IHandleMessages<PaymentMessage>
+    {
+        private readonly IBus bus;
+
+        public PayHandlers(IBus bus)
+        {
+            this.bus = bus;
+        }
+
+        public void Handle(PaymentMessage message)
+        {
+            Console.WriteLine("======================================================================");
+            Console.WriteLine(message.EventId);
+            Console.WriteLine("======================================================================");
+
+            bus.Return((int)ErrorCodes.None);
+
+
+        }
+
+    }
+}
